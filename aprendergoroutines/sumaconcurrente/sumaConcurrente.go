dec2a38d8709a1988b0d9ac7f0718ca4fd3a9eb0
manuelguirado <manu22.mgb@gmail.com>
2025-03-21 12:39:55 +0100

    aprendiendo sobre go routines y channel

---
+++ aprendergoroutines/sumaconcurrente/sumaConcurrente.go
@@ -0,0 +1,24 @@
+package main
+import (
+	"fmt"
+	"time"
+
+)
+
+func sumar (numbers[]int, ch chan int) {
+	sum := 0
+	for _, v := range numbers {
+		sum += v
+	}
+	ch <- sum // mandar la informacion de sum a channel previamente definido como ch 
+}
+func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	ch := make(chan int)
+	go sumar(numbers[:len(numbers)/2] , ch) //divide el array en dos partes y los suma de manera concurrente 
+	go sumar(numbers[len(numbers)/2:] , ch) //esta es la 2 mitad del array
+	sum1, sum2 := <-ch, <- ch
+	fmt.Println("suma total" , sum1 +  	sum2) 
+	time.Sleep(1 * time.Second)
+
+}
